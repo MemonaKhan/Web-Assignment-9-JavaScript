346b73c851dc9dbf195eec1083d695d2282a7054
MemonaKhan <you@example.comsuhail1975pk@yahoo.co.uk>
2020-07-14 23:48:55 +0500

    completed starting tasks

---
+++ app.js
@@ -0,0 +1,46 @@
+// // Assignment # 38-42
+
+// // Task # 1
+
+// function power(a,b){
+//     var ans = 1;
+//     for(var i = 1; i<=b; i++){
+//         ans = ans*a;
+//     }
+//     return ans;
+// }
+
+// alert("3 raise to the power 4 is "+power(3,4));
+
+
+// // Task # 2
+
+// function checkLeapYear(year){
+//     if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+//         return "Leap Year";
+//     }
+//     else{
+//         return "not a Leap Year";
+//     }
+// }
+
+// var year = prompt("Enter the year","Year hear");
+// alert(year + " is "+checkLeapYear(year));
+
+
+// // Task # 3
+
+
+
+
+// // Task # 4
+// // Task # 5
+// // Task # 6
+// // Task # 7
+// // Task # 8
+// // Task # 9
+// // Task # 10
+// // Task # 11
+// // Task # 12
+// // Task # 13
+// // Task # 14
